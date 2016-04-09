cc0544c6c1f3b96b4b8c25b0f4c47df53c166231
Andre Cardote <acardote@Andres-MacBook-Pro-2.local>
2016-04-09 03:01:27 +0100

    Added /pomocancel. MVP 1

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var http = require('http');
 var redis = require('redis');
 var qs = require('querystring');
+var Slack = require('slack-node');
 
 
 const PORT = 8080;
 
-// Setup redis
+
+// Setup redis connection
 rClient = redis.createClient();
 rClient.on("error", function (err) {
     console.log("Error " + err);
 });
+
+// Setup slack api connection
+apiToken = "---API Token---";
+slack = new Slack(apiToken);
 
 // Handle requests and send response
 function handleRequest(request, response) {
@@ -33,15 +39,21 @@ function handleRequest(request, response) {
             // Start pomodoro with default timer
             if (postData.command === '/pomostart') {
                 if (postData.text === '') {
-                    rClient.set('@'+postData.user_name, 'inPomo', redis.print);
-                    rClient.expire('@'+postData.user_name, 15, redis.print);
-                    
+                    rClient.set('@' + postData.user_name, 'inPomo', redis.print);
+                    rClient.expire('@' + postData.user_name, 50*60, redis.print);
+
                     response.writeHead(200, {
                         'Content-Type': 'text/html'
                     });
-                    response.end('Your pomodoro has just started. See you again in 50 minutes!');
+                    response.end('Your pomodoro has just started, I\'ve just disabled your notifications for 50 minutes. See you soon!');
 
                     // Use slack API to set user's notifications off for x amount of time
+                    slack.api('dnd.setSnooze', {
+                        num_minutes: '50'
+                        , user_name: postData.user_name
+                    }, function (err, response) {
+                        console.log(response);
+                    });
                 }
                 // Start pomodoro with custom timer
                 else {
@@ -60,8 +72,8 @@ function handleRequest(request, response) {
                             response.writeHead(200, {
                                 'Content-Type': 'text/html'
                             });
-                            response.end(postData.text + ' is in a pomodoro until ' + time + ' seconds from now.');
-                            console.log(postData.text + ' is in a pomodoro until ' + time + ' seconds from now.');
+                            response.end(postData.text + ' is in a pomodoro until ' + Number(time/60).toFixed(0) + ' minutes from now.');
+                            console.log(postData.text + ' is in a pomodoro until ' + Number(time/60).toFixed(0) + ' minutes from now.');
                         })
 
                     } else {
@@ -74,6 +86,20 @@ function handleRequest(request, response) {
                     }
                 });
 
+            } else if (postData.command === '/pomocancel') {
+                // Delete the key from the database
+                rClient.del('@' + postData.user_name, 'inPomo', redis.print);
+                
+                // Cancel notif snooze
+                slack.api('dnd.endSnooze', {
+                        user_name: postData.user_name
+                    }, function (err, response) {
+                        console.log(response);
+                    });
+                
+                // Reply
+                response.end('I\'ve cancelled your pomodoro. Hope it\'s for a good reason!');
+                console.log('I\'ve cancelled your pomodoro. Hope it\'s for a good reason!');
             }
         });
     } else {
